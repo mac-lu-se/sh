1a7651613ab64dbb28c3dbc90c494b6c9216bccb
Johan Dahl <Johan.Dahl@lth.lu.se>
2021-10-28 12:35:23 +0200

    change from default export again

--- mod.js
+++ mod.js
@@ -1,17 +1,5 @@
-import { currentUser, locale, lastUser } from './user.js'
-import { run, runScriptAsCurrentUser } from './run.js'
-import { addLine, fileInfo } from './file.js'
-import { showDialog } from './dialog.js'
-import { formatDate } from './datetime.js'
+export { currentUser, locale, lastUser } from './user.js'
+export { run, runScriptAsCurrentUser } from './run.js'
+export { addLine, getStat } from './file.js'
+export { showDialog } from './dialog.js'
 
-export default {
-  addLine,
-  currentUser,
-  fileInfo,
-  formatDate,
-  lastUser,
-  locale,
-  run,
-  runScriptAsCurrentUser,
-  showDialog
-}
