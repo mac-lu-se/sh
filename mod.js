b7ae003ffc9ca6b12d6479bf7187e96df3c29c95
Johan Dahl <Johan.Dahl@lth.lu.se>
2021-10-28 10:57:40 +0200

    0.2 Fix: export everything

--- mod.js
+++ mod.js
@@ -1,4 +1,15 @@
 import { currentUser, locale, lastUser } from './user.js'
 import { run, runScriptAsCurrentUser } from './run.js'
-import { addLine, getStat } from './file.js'
+import { addLine, fileInfo } from './file.js'
 import { showDialog } from './dialog.js'
+
+export {
+  addLine,
+  currentUser,
+  fileInfo,
+  lastUser,
+  locale,
+  run,
+  runScriptAsCurrentUser,
+  showDialog
+}
